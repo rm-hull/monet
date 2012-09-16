e389c458e4c35dbedf0a74c7cfeeb2f52dd6a01c
Richard Hull <rm_hull@yahoo.co.uk>
2012-09-16 23:20:37 +0100

    Project settings

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject monet "0.1.0-SNAPSHOT"
+(defproject org.clojars.rm-hull/monet "0.1.1-SNAPSHOT"
   :description "A ClojureScript visual library"
-  :dependencies [[clojure "1.3.0"]])
+  :url "http://github.com/rm_hull/monet"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
