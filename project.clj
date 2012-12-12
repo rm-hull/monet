a506cda13d2aebf38b5383a39b3003438b0e9883
Osbert Feng <ofeng@ozbert.com>
2012-12-11 16:16:58 -0800

    Update cljsbuild and clojurescript dependencies to satisfy lein pedantic.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject monet "0.1.0-SNAPSHOT"
   :description "A ClojureScript visual library"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1236"]]
-  :plugins [[lein-cljsbuild "0.2.1"]]
+                 [org.clojure/clojurescript "0.0-1535"]]
+  :plugins [[lein-cljsbuild "0.2.9"]]
   :hooks [leiningen.cljsbuild]
   :source-path "no-clj-here"
   :cljsbuild {:builds
