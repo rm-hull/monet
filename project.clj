ef3bdf71ea40eda39461dda0dbb0fbf179b8259b
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-27 20:31:32 +0000

    Versions

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject rm-hull/monet "0.1.6"
+(defproject rm-hull/monet "0.1.7"
   :description "A ClojureScript visual library"
   :url "https://github.com/rm_hull/monet"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1586"]]
   :plugins [[lein-cljsbuild "0.2.9"]]
   :hooks [leiningen.cljsbuild]
   :source-path "no-clj-here"
   :cljsbuild {
     :builds [{:source-paths ["src"], :id "main", :jar true}]}
-) 
+  :min-lein-version "2.1.1"
+  :warn-on-reflection true) 
